0598c92794ac2cf29cd50db546f5cfa6afc6b3a0
resu-xuniL <linux@lavache.com>
2024-05-04 21:15:05 +0200

    Get commit message from file + cosmetic changes + delete useless files

--- up.sh
+++ up.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
-#set -e
-
-# Undo last commit
-# https://www.git-tower.com/learn/git/faq/undo-last-commit
-
-# reset - commit your changes or stash them before you merge
-# git reset --hard - ArcoLinux alias - grh
-
-# reset - go back one commit - all is lost
-# git reset --hard HEAD~1
-
-
-# remove a file online but keep it locally
-# https://www.baeldung.com/ops/git-remove-file-without-deleting-it
-# git rm --cached file.txt
-
 
 # Creating the databases
-cd x86_64
-sh update_repo.sh
-cd ..
-
-#workdir=$(pwd)
+rm x86_64/arcolinux-settings_repo*
+repo-add -n -R x86_64/arcolinux-settings_repo.db.tar.gz x86_64/*.pkg.tar.zst
 
 # Below command will backup everything inside the project folder
 git add --all .
 
-echo "####################################"
-echo "Write your commit comment!"
-echo "####################################"
+# Set commit message
+input=$(ls x86_64 | grep '.pkg.tar.zst' | cut -d '-' -f 4,5)
 
-read input
+tput setaf 3
+echo " Commit message will be : Release ${input}"
+tput sgr0
 
 # Committing to the local repository with a message containing the time details and commit text
-
-git commit -m "$input"
+git commit -m "Release ${input}"
 
 # Push the local files to github
-
 git push -u origin main
-#git push -u origin main --force
-
-echo "################################################################"
-echo "###################    Git Push Done      ######################"
-echo "################################################################"
